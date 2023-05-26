fd3a08bd33708e86706f9a2448623f09e9bd7a44
yoshiki-nakamura <yoshiki.nakamura@automagi.jp>
2023-05-27 03:14:35 +0900

    API Route

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,10 +1,25 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Main (main) where
 
 import Web.Scotty
+import Network.HTTP.Types.Status
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics (Generic)
+import qualified Data.Text as T
 
-main = scotty 8080 $
-  get "/:word" $ do
-    beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+data Error = Error { message :: T.Text } deriving (Generic, Show)
+instance ToJSON Error
+
+main :: IO ()
+main = scotty 8080 routes
+
+routes :: ScottyM ()
+routes = do
+  get "/api/users" $ do
+    status status200
+    html $ mconcat ["<h1>Scotty API</h1>"]
+  notFound $ do
+    status status404
+    json Error {message = "API URI not found."}
