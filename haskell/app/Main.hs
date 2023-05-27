f312c3fe6c5ee45c322901754985cda666d62550
yoshiki-nakamura <yoshiki.nakamura@automagi.jp>
2023-05-27 19:58:00 +0900

    5/27 - 2

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -7,7 +7,6 @@ import Network.HTTP.Types.Status
 import qualified Data.Aeson as Aeson
 import Data.Aeson ((.=))
 import Control.Monad.IO.Class (liftIO)
-import Common.Types
 import Cat.Model
 
 main :: IO ()
@@ -15,13 +14,17 @@ main = scotty 8080 routes
 
 routes :: ScottyM ()
 routes = do
-  get "/api/cat" $ getCats2
+  get "/api/cat" $ do
+    status status200
+    cats <- (liftIO getCats)
+    json $ Aeson.object ["cats" .= cats]
+
   notFound $ do
     status status404
-    json ErrorMessage {message = "API URI not found."}
+    json $ Aeson.object ["message" .= ("API URI not found." :: String)]
 
-getCats2 :: ActionM ()
-getCats2 = do
-  status status200
-  cats <- (liftIO getCats)
-  json $ Aeson.object ["cats" .= cats]
+-- getCats2 :: ActionM ()
+-- getCats2 = do
+--   status status200
+--   cats <- (liftIO getCats)
+--   json $ Aeson.object ["cats" .= cats]
